d27f3c1078de9db7253c9f27d0510e496996f4bf
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-04-03 00:43:54 +0300

    componet input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,21 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name = 'Katsivelos';
 
+  person0  = {
+    givenName: 'Stefanos',
+    surName: 'Katsivelos',
+    age: 34,
+    email: 'stef.katsivelos@gmail.gr',
+    address: 'Athens, Greece'
+  };
+
+  person1 = {
+    givenName: 'Brian',
+    surName: 'Greene',
+    age: 0x37,
+    email: 'Brian.Greene@gmail.gr',
+    address: 'New, York'
+  }
+
  
 }
