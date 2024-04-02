de31187be4d06f4d93f651dff1d54bb630d6ecc4
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-04-02 23:27:04 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Katsivelos';
 
-  person = {
-    givenName: 'Stefanos',
-    surName: 'Katsivelos',
-    age: 34,
-    email: 'stef.katsivelos@gmail.gr'
-  }
+ 
 }
