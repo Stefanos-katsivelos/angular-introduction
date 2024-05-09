17193e7d3d4207a486e83d0ae634a75db5a1624e
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-05-09 21:41:26 +0300

    http client primer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import {
+  provideHttpClient,
+  withInterceptorsFromDi
+
+} from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+  ]
 };
